7c4bad3e0463d10d490b07af3bd917a4e0675f7b
Axure <axurez@vip.qq.com>
2016-06-20 01:07:39 +0800

    Add some filesystem operations and tests.

--- src/main/java/com/zjuqsc/database/Database.java
+++ src/main/java/com/zjuqsc/database/Database.java
@@ -1,5 +1,9 @@
 package com.zjuqsc.database;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
@@ -7,11 +11,25 @@ import java.util.concurrent.Future;
  * Created by zhenghu on 2016-06-19.
  */
 public class Database extends AbstractDatabaseSerializable {
+    private final static String userHomePath = System.getProperty("user.home");
+    private final String rootPath;
+    private final static String dbDirectorySuffix = ".ydb";
+
     private final String name;
     private BufferManager<FourKBlockBuffer> bufferManager;
-    private final ForkJoinPool pool = new ForkJoinPool();
+    private transient final ForkJoinPool pool = new ForkJoinPool();
+
+    private transient Path directory;
 
     public Database(String name) {
+        this.rootPath = userHomePath;
+        this.name = name;
+        this.bufferManager = new DatabaseBufferManager();
+    }
+
+
+    public Database(String name, String rootPath) {
+        this.rootPath = rootPath;
         this.name = name;
         this.bufferManager = new DatabaseBufferManager();
     }
@@ -22,6 +40,19 @@ public class Database extends AbstractDatabaseSerializable {
 
     public void persistAll() {
 
+    }
+
+    public void persistDirectory() throws IOException {
+        File homeDirectory = new File(rootPath);
+        File dbDirectory = new File(homeDirectory, dbDirectorySuffix);
+        if (dbDirectory.isFile()) {
+            throw new IOException("Directory already exist as a file!");
+        }
+        if (dbDirectory.isDirectory()) {
+
+        } else {
+            Files.createDirectories(dbDirectory.toPath());
+        }
     }
 
     public void persistDatabase() {
@@ -37,7 +68,7 @@ public class Database extends AbstractDatabaseSerializable {
         return 5;
     }
 
-    public Future<byte[]> serializeTo(byte[] byteBuffer)  {
+    public Future<byte[]> serializeTo(byte[] byteBuffer) {
         Future<byte[]> future = pool.submit(() -> {
             int length = getSerializeLength();
             for (int i = 0; i < length; ++i) {
