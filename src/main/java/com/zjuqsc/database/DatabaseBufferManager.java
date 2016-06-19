9641825d5a3b439e141cd8f9cfb50d3b44b73351
Axure <axurez@vip.qq.com>
2016-06-19 23:17:31 +0800

    Add generation management for buffer manager.

--- src/main/java/com/zjuqsc/database/DatabaseBufferManager.java
+++ src/main/java/com/zjuqsc/database/DatabaseBufferManager.java
@@ -1,6 +1,7 @@
 package com.zjuqsc.database;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -14,10 +15,24 @@ public class DatabaseBufferManager implements BufferManager<FourKBlockBuffer> {
     private static final int size = 5000;
 
     private PriorityQueue<Integer> handleHoles;
+    private HashMap<Integer, Boolean> isLocked;
+    private HashMap<Integer, FourKBlockBuffer> buffersToKill;
     private int largestIndex = 0;
+
+    public void tryToRemoveHandle(int handle, FourKBlockBuffer value) {
+        if (isLocked.get(handle) != null && isLocked.get(handle)) {
+            buffersToKill.put(handle, value);
+        } else {
+            removeHandle(handle);
+        }
+    }
 
     protected void removeHandle(int handle) {
         handleHoles.add(handle);
+    }
+
+    protected void killHandle(int handle) {
+        buffersToKill.remove(handle);
     }
 
 //    protected void useHandle(int handle) {
@@ -48,7 +63,7 @@ public class DatabaseBufferManager implements BufferManager<FourKBlockBuffer> {
         protected boolean removeEldestEntry(Map.Entry<Integer, FourKBlockBuffer> eldest) {
             if (size() > size) {
                 // May need some lock.
-                removeHandle(eldest.getKey());
+                tryToRemoveHandle(eldest.getKey(), eldest.getValue());
                 return true;
             }
             return false;
@@ -64,10 +79,10 @@ public class DatabaseBufferManager implements BufferManager<FourKBlockBuffer> {
     }
 
     int loadBuffer(FilePointer filePointer) throws IOException {
-        pointer++;
+//        pointer++;
         int handle = getAvailableHandle();
 
-        buffers.put(handle, new FourKBlockBuffer(filePointer));
+        put(handle, new FourKBlockBuffer(filePointer));
         return handle;
     }
 
@@ -75,6 +90,34 @@ public class DatabaseBufferManager implements BufferManager<FourKBlockBuffer> {
     @Override
     public DatabaseBuffer load() {
         return null;
+    }
+
+    public void lock(int handle) {
+        isLocked.put(handle, true);
+    }
+
+    public void release(int handle) {
+        isLocked.remove(handle);
+        if (buffersToKill.get(handle) != null) {
+            buffersToKill.remove(handle);
+        }
+    }
+
+    public boolean isLocked(int handle) {
+        return isLocked.get(handle) == null && isLocked.get(handle);
+    }
+
+    public FourKBlockBuffer get(int handle) {
+        FourKBlockBuffer result = buffers.get(handle);
+        if (result == null) {
+            result = buffersToKill.get(handle);
+        }
+        return result;
+    }
+
+    public FourKBlockBuffer put(int handle, FourKBlockBuffer value) {
+        buffers.put(handle, value);
+        return value;
     }
 }
 
